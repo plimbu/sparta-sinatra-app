e123bdfa7157f72e45369bc6e27a04e0308f46cc
Peter Limbu <peterlimbu@spartaglobal.com>
2018-07-20 10:15:38 +0100

    added more indentation

--- public/js/global.js
+++ public/js/global.js
@@ -1,20 +1,14 @@
-$(function(){
+$(function() {
 
 
-    function generateRandomForBG() {
-        var num = Math.floor(Math.random() * 3);
-        return num;
-    }
-    setInterval(function(){
+  function generateRandomForBG() {
+    var num = Math.floor(Math.random() * 3);
+    return num;
+  }
+  setInterval(function() {
       var number = generateRandomForBG().toString();
 
-      $("body").css("background-image", "url('../img/bg"+number+".jpg')").fadeIn();
-
-
+      $("body").css("background-image", "url('../img/bg" + number + ".jpg')").fadeIn();
     }, 900
-
-
-    )
-
-
+  )
 });
