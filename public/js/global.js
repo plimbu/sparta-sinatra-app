9dad37a20cc8d6a759f25a68422276ccb4100285
Peter Limbu <peterlimbu@spartaglobal.com>
2018-07-20 08:33:33 +0100

    fully functional buttons with minimal presentation

--- public/js/global.js
+++ public/js/global.js
@@ -1,6 +1,20 @@
 $(function(){
 
-$("body").append($("<p> Jquery is working</p>"));
+
+    function generateRandomForBG() {
+        var num = Math.floor(Math.random() * 3);
+        return num;
+    }
+    setInterval(function(){
+      var number = generateRandomForBG().toString();
+
+      $("body").css("background-image", "url('../img/bg"+number+".jpg')").fadeIn();
+
+
+    }, 900
+
+
+    )
 
 
 });
